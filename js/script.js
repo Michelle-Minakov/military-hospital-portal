965e5886637453ce08f07aa7ee91a1c63dc34dd1
Michelle-Minakov <teresukm339@gmail.com>
2025-11-26 04:53:09 +0200

    initialcommit: Military Hospital portal

--- js/script.js
+++ js/script.js
@@ -1,40 +1,11 @@
-$(function() {
-  // JQueryUI datepicker
-  $("#admissionDate").datepicker({
-    dateFormat: "dd.mm.yy"
-  });
 
-  // Додавання новини
-  $("#newsForm").on("submit", function(e) {
-    e.preventDefault();
-    const title = $("#newsTitle").val();
-    const content = $("#newsContent").val();
-    $("#newsList").append(`
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">${title}</h5>
-            <p class="card-text">${content}</p>
-          </div>
-        </div>
-      </div>
-    `);
-    this.reset();
-  });
-
-  // Додавання пацієнта
-  $("#patientForm").on("submit", function(e) {
-    e.preventDefault();
-    const name = $("#patientName").val();
-    const diagnosis = $("#patientDiagnosis").val();
-    const date = $("#admissionDate").val();
-    $("#patientList").append(`
-      <tr>
-        <td>${name}</td>
-        <td>${diagnosis}</td>
-        <td>${date}</td>
-      </tr>
-    `);
-    this.reset();
-  });
+// Додавання довідки
+$("#addDocBtn").on("click", function() {
+  const docName = $("#newDoc").val();
+  if (docName) {
+    $("#docsList").append(`<li class="list-group-item">${docName}</li>`);
+    $("#newDoc").val("");
+  }
+ 
 });
+
